268088feb920c6070f7f3580a45982b915365467
odgwtg <143929611+odgwtg@users.noreply.github.com>
2024-01-12 23:37:33 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 #请修改为自己的UUID
-export UUID=e4c40e74-8352-4bba-a36c-10349334eba4
+export UUID=d4f048c8-dd84-4b15-a00f-a93b6585b276
 
 #请修改为自己设置的伪装站，不要带https://
-export ProxySite=techtanker.com
+export ProxySite=hoop-light.com
 
 #端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
 export Port=8080
